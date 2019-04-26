e3721a18df5308f77ee27d9d0b52678414154a71
AurorePaladin <aurorepaladin@gmail.com>
2019-04-27 03:10:12 +0800

    切换任务状态

--- pages/todos/todos.js
+++ pages/todos/todos.js
@@ -9,7 +9,8 @@ Page({
       { name: 'Learning CSS', completed: false },
     ]
   },
-
+  // 拿到文本框里面的值
+  // 由于小程序的数据绑定是单向的 必须要给文本注册改变事件
   inputChangeHandle: function (e) {
     this.setData({ search: e.detail.value })
   },
@@ -23,11 +24,15 @@ Page({
       name: this.data.search,
       completed: false
     })
+    // 将这个值添加到列表中
     // 必须显式的通过setData去改变数据，这样界面上才会得到变化
     this.setData({ todos: todos, search: '' })
-  }
+  },
 
-  // 拿到文本框里面的值
-  // 由于小程序的数据绑定是单向的 必须要给文本注册改变事件
-  // 将这个值添加到列表中
+  toggleTodoHandle: function (e) {
+    // 切换当前点中的item的完成状态
+    var item = this.data.todos[e.currentTarget.dataset.index]
+    item.completed = !item.completed
+    this.setData({ todos: this.data.todos })
+  }
 })
